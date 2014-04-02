311adf6db867973c039b07174ebd0d51f1e870d0
Simon Benfeldt <simonbenfeldt@gmail.com>
2014-04-02 14:09:46 +0200

    HydrateMe v0.0

---
+++ HydrateMe/HydrateMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HydrateMe
+//
+//  Created by Mads Engels on 4/2/14.
+//  Copyright (c) 2014 UNIGULD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
