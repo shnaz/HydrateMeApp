7c6efe032a3fbf5f524f70aedc67098a5f6428a3
shnaz <shafi@live.dk>
2014-04-05 23:50:11 +0200

    - added stat screen with view controller

---
+++ HydrateMe/HydrateMe/StatScreenViewController.h
@@ -0,0 +1,15 @@
+//
+//  StatScreenViewController.h
+//  HydrateMe
+//
+//  Created by Shafi on 05/04/14.
+//  Copyright (c) 2014 UNIGULD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StatScreenViewController : UIViewController
+- (IBAction)button1:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *label1;
+
+@end
